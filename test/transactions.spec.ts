00abf8d187bdb2d56dbd7b177412fcfb6cbc3c6f
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2023-09-26 21:44:13 -0300

    Add files via upload

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { it, beforeAll, afterAll, describe } from 'vitest'
+import { expect, it, beforeAll, afterAll, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -12,7 +12,7 @@ describe('Transaction routes', () => {
     })
 
     it('should be able to create a new transaction', async () => {
-        const response = await request(app.server)
+        await request(app.server)
             .post('/transactions')
             .send({
                 title: 'New transaction',
@@ -20,6 +20,32 @@ describe('Transaction routes', () => {
                 type: 'credit'
             })
             .expect(201)
-    })   
+        }
+    )
+
+    it('should be able to list all transactions', async () => {
+        const createTransactionResponse = await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'New transaction',
+                amount: 5000,
+                type: 'credit'
+            })
+
+            const cookies = createTransactionResponse.get('Set-Cookie')
+
+            const listTransactionResponse = await request(app.server)
+                .get('/transactions')
+                .set('Cookie', cookies)
+                .expect(200)
+            
+            expect(listTransactionResponse.body.transactions).toEqual([
+                expect.objectContaining({
+                    title: 'New transaction',
+                    amount: 5000,
+                }),
+            ])
+        }
+    )
 })
 
